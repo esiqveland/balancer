7afd604851f3c7ffe5f4d86c3e9dfa9ca97018e7
Eivind Siqveland Larsen <eivind@siqve.land>
2019-04-09 23:58:44 +0200

    Update balancer.go

--- balancer.go
+++ balancer.go
@@ -5,8 +5,10 @@ import (
 	"net"
 )
 
+// ErrNoHosts if the lookup returned 0 hosts.
 var ErrNoHosts = errors.New("No hosts available in list")
 
+// Balancer returns the next Host to use.
 type Balancer interface {
 	Next() (Host, error)
 }
