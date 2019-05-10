9e6d6551eb109d16687b76f1037fb6fd2a8b4337
Eivind Siqveland Larsen <eivind@siqve.land>
2019-05-10 23:32:43 +0200

    srvbalancer: idea for writing tests

--- netbalancer/srvbalancer_test.go
+++ netbalancer/srvbalancer_test.go
@@ -14,3 +14,16 @@ func TestResolver(t *testing.T) {
 	require.NotNil(t, opt)
 }
 
+type mockLookup struct {
+	sleeptime  time.Duration
+	resultsSRV func() (cname string, addrs []*net.SRV, err error)
+	resultsIP  func() ([]net.IPAddr, error)
+}
+
+func (m *mockLookup) LookupSRV(ctx context.Context, service, proto, name string) (cname string, addrs []*net.SRV, err error) {
+	return m.resultsSRV()
+}
+
+func (m *mockLookup) LookupIPAddr(ctx context.Context, host string) ([]net.IPAddr, error) {
+	return m.resultsIP()
+}
